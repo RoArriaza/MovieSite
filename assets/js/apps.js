18e53941796df7a160e0f6201e859bf1446b594b
berenisser <berenisser@gmail.com>
2018-01-09 17:25:48 -0300

    actualización nav y carrusel

---
+++ assets/js/apps.js
@@ -0,0 +1,5 @@
+   $(document).ready(function(){ 
+   		$(".button-collapse").sideNav();// Inicialización menú hamburguesa
+      $('.slider').slider(); // Inicialización código carrusel media
+    });
+        
