8b2b2284a2ca67a5f36df9ccb6566b1e3caff478
macaraos <macarena.araos.p@gmail.com>
2018-01-10 01:07:31 -0300

    actualizado inicial.css inicial.html e inical.js

--- assets/js/inicial.js
+++ assets/js/inicial.js
@@ -1,42 +1,6 @@
-*codigo para crear post
-*/
-var botonLlamar = document.getElementById("btn");
-botonLlamar.addEventListener("click", rescatarMensaje);
+ $(document).ready(function(){
+	$('.carousel').carousel();
 
-/**
-*Rescatando mensaje de ingreso
-*/
-function rescatarMensaje(){
+ });
 
-	var comment = document.getElementById("comment").value;
-	document.getElementById("comment").value = " ";
 
-/**
-*Llamando a boton
-*/
-	var btn = document.getElementById("btn");
-
-/**
-*Creando contenedor para mensajes
-*/
-	var mensaje = document.createElement("div");
-	mensaje.classList.add("divNuevo");
-
-	var texto = document.createElement("p");
-
-/**
-*Llamando a contenedor de mensajes
-*/
-	var cont = document.getElementById("cont2");
-	var textTwo = document.createTextNode(comment);
-
-/**
-*Dandole a cada padre su hijo
-*/
-	texto.appendChild(textTwo);
-	mensaje.appendChild(texto);
-	cont2.appendChild(mensaje);
-}
-/**
-*aqui termina el codigo para crear post
-*/
