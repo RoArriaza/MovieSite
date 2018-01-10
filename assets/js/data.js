5104e8b6c2c95a1625ad3c92c4a30371b1d4ba8e
RoArriaza <ro.arriazasilva@gmail.com>
2018-01-10 16:17:56 -0300

    data2

--- assets/js/data.js
+++ assets/js/data.js
@@ -9,8 +9,28 @@ var data = [
   "id": "tt0241527"
   },
   {
+  "nombre": " Harry Potter y la cámara secreta",
+  "ebook": "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=harry+potter+ebook",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía, Familiar",
+  "trailer": "",
+  // Buscar trailer de película con subtítulos si es en inglés.
+  "id": "tt0295297"
+  },
+  {
+  "nombre": "Harry Potter y el prisionero de Azkaban",
+  "ebook": "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=harry+potter+ebook",
+  "libro": "",
+  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
+  "genero": "Fantasía", "Familiar",
+  "trailer": "",
+  // Buscar trailer de película con subtítulos si es en inglés.
+  "id": "tt037699"
+  },
+  {
   "nombre": "",
-  "ebook": "",
+  "ebook": "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=harry+potter+ebook",
   "libro": "",
   // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
   "genero": "Fantasía, Familiar",
@@ -19,28 +39,8 @@ var data = [
   "id": ""
   },
   {
-  "nombre": "",
-  "ebook": "",
-  "libro": "",
-  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
-  "genero": "Fantasía, Familiar",
-  "trailer": "",
-  // Buscar trailer de película con subtítulos si es en inglés.
-  "id": ""
-  },
-  {
-  "nombre": "",
-  "ebook": "",
-  "libro": "",
-  // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
-  "genero": "Fantasía, Familiar",
-  "trailer": "",
-  // Buscar trailer de película con subtítulos si es en inglés.
-  "id": ""
-  },
-  {
-  "nombre": "",
-  "ebook": "",
+  "nombre": "Harry Potter y el prisionero de Azkaban",
+  "ebook": "https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=harry+potter+ebook",
   "libro": "",
   // Poner el resultado de búsqueda de la librería para que el usuario tenga opción de elegir cuál edición desea comprar.
   "genero": "Fantasía, Familiar",
