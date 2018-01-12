75071073b1fbddb27a02eb28c094d750e9668eaa
mthompsonc <m.paz.thompson@gmail.com>
2018-01-12 10:25:42 -0300

    Ordenando últimos detalles

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -231,13 +231,15 @@ function renderMovies (response) {
     function movieInfo(paste) {
       var poster= $('#poster');
       var datos= $('#datos');
-      poster.append('<img src="' + paste.Poster + '" alt="poster">');
-      datos.append('<h3>' + paste.Title + '</h3> <h4>Año:</h4><p>'
+      var rating= $('#rating');
+      poster.append('<img src="' + paste.Poster + '" alt="poster">')
+      datos.append('<h3>' + paste.Title + '</h3> <a href="'
+                  + paste.Website + '">Website Oficial</a><h4>Año:</h4><p>'
                   + paste.Year + '</p><h4>Duración: </h4><p>' + paste.Runtime
                   + '</p><h4>Director:</h4><p>' + paste.Director
                   + '</p><h4>Escritor: </h4><p>' + paste.Writer
                   + '</p><h4>Actores: </h4><p>' + paste.Actors
-                  + '</p><a href="' + paste.Website + '">Website Oficial</a>');
+                  + '</p>');
     }
   });
 
